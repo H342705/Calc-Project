73283476b9d273937f3ef4f0040264b8f2bb89eb
Hamza Rahim <hamzarahim836@gmail.com>
2023-08-30 13:04:12 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+let { num1, num2, operation } = await inquirer.prompt([
+    {
+        name: "num1",
+        type: "number",
+        message: "Enter first number:"
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "Enter second number:"
+    },
+    {
+        name: "operation",
+        type: "list",
+        choices: ["addtion", "subtraction", "multiplication", "division"],
+        messsage: "Enter your operation"
+    }
+]);
+if (operation === "addition") {
+    console.log(`The sum of two number = ${num1 + num2}`);
+}
+else if (operation === "subtraction") {
+    console.log(`The difference of two number = ${num1 - num2}`);
+}
+else if (operation === "multiplication") {
+    console.log(`The multiplication of two number = ${num1 * num2}`);
+}
+else if (operation === "division") {
+    console.log(`The division of two number = ${num1 / num2}`);
+}
